5051bd03e9bb55788f53b3ed209603cc455d97c2
Shozon Raj <45089799+moshiuzzaman@users.noreply.github.com>
2023-05-28 22:27:06 +0600

    refactor: cors, perser, mongoose setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+
+const bootstrap = async () => {
+    try {
+        await mongoose.connect(config.databaseUrl as string);
+        console.log("✅ Database connect successfully");
+
+        app.listen(config.port, () => {
+            console.log(`🏃‍♂️  App listening on port ${config.port}`)
+        })
+
+
+    } catch (error) {
+        console.log("❌ Database connection faild");
+
+    }
+}
+bootstrap()
