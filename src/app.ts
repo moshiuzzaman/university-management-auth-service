354503d59f3163cad1bc5a069c211212ba16d479
Shozon Raj <45089799+moshiuzzaman@users.noreply.github.com>
2023-05-29 17:47:16 +0600

    refactor: prettier,eslint setup, huskey and lint-staged satup

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-import express, { Application, Request, Response } from "express";
-import cors from "cors"
-
-
-
-const app:Application = express()
+const app: Application = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('😊 App works fine')
 })
-
-
 
 export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv"
-import path from "path"
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({path:path.join(process.cwd(),".env")})
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT,
-    databaseUrl:process.env.DATABASE_URL
+  port: process.env.PORT,
+  databaseUrl: process.env.DATABASE_URL,
 }
